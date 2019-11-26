a065283a14ed04ad66cf9704b77c1b776da673ad
Pengbo <40770311+mapengbo0314@users.noreply.github.com>
2019-11-26 15:47:27 -0600

    Trying to make toggle for read, but was able to get addform to hide and show.
    convereted the constructor of our books to a const, so it's easier to access

--- script.js
+++ script.js
@@ -1,36 +1,70 @@
 let myLibrary = [];
 
-function Books(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
-  read = read ? "read" : "not read";
-  return { title, author, pages, read };
+
+const Books = (title, author, pages, read) => {
+  const info = () => {
+    return `${title}, by ${author}, ${pages} pages, ${read ? 'read' : 'not read yet'}`;
+  }
+
+  const toggleReadBook = () => {
+    read = !read;
+  }
+
+  return { title, author, pages, read, info, toggleReadBook }
 }
 
-function addBookToLibrary(book) {
+function addBookToLibrary(book) { //adding a book to the library
   myLibrary.push(book);
 }
 
-const theHobbit = new Books("The Hobbit ", "J.R.R. Tolkien ", 295, false);
-const wrap = document.querySelector("#newBookForm li .text");
-console.log(wrap);
-addBookToLibrary(theHobbit);
-console.log(myLibrary);
+function toggleBookForm() {
+  document.querySelector('#newBookForm').classList.toggle('box');
+}
+function addEventHandlers() {
+  renderBookFormHandler();
+}
+function renderBookFormHandler() {
+  const newBookFormButton = document.querySelector('#newBookFormButton');
+  newBookFormButton.onclick = () => toggleBookForm();
+}
+
+
 
 function init() {
   renderBookList();
 }
 
 function renderBookList() {
+  let text = ``;
   let bookList = document.getElementById("booklist");
-  var text = "";
-  Object.values(myLibrary[0]).forEach(function(k) {
-    text += k + " ";
-  });
-  bookList.innerHTML = text;
-  console.log(text);
+
+  bookList.innerHTML = myLibrary.map((book, index) => {
+    console.log(index);
+    return '<div class="book-card" data-library-index="' + index + '"><div class="book-info">' + book.info() + '</div><button class="read-book">' + (!book.read ? "I've read this book" : "I haven't read this book") + '</button><button class="delete-book">Remove book from my library</button></div>';
+
+  }).join('');
+  addEventHandlers();
 }
+const theHobbit = Books("The Hobbits", "J.R.R. Tolkien", 295, false);
+const theHobbit1 = Books("The Hobbits1", "J.R.R. Tolkien", 295, false);
+const theHobbit2 = Books("The Hobbits2", "J.R.R. Tolkien", 295, false);
+
+addBookToLibrary(theHobbit);
+addBookToLibrary(theHobbit1);
+addBookToLibrary(theHobbit2);
 
 init();
+
+
+
+
+
+
+
+
+
+// const { title, author, pages, read } = myLibrary[0];  //This is very useful for string accessing
+// let text = `${title},  ${author}, ${pages} pages, <button id= "readjs" style = "padding: 5px" onclick = "toggleRead()">${read}</button>`;
+//   // Object.values(myLibrary[0]).forEach(function (k) {  //this way works as it is iterating through the object and finding the values to put for each
+//   //   text += k + " "; //This gives us the ability to add all words on there but with no string manuplation 
+//   // });
